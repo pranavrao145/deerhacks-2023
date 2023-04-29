3244d6b251c9b015597eafe319dbd0d5cadefb8f
Shelley Chen <shelleychen318@gmail.com>
2023-04-29 15:48:59 -0400

    set up wardrobe item details page

--- frontend/src/pages/wardrobeItem/index.js
+++ frontend/src/pages/wardrobeItem/index.js
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { HiOutlineArrowNarrowLeft } from "react-icons/hi";
+import { RiDeleteBinLine } from "react-icons/ri";
 import WardrobeItemDetails from "../../components/WardrobeItemDetails";
 import styles from "./wardrobeItem.module.scss";
 
@@ -10,12 +11,22 @@ export default function WardrobeItem() {
     navigate("/wardrobe");
   };
 
+  // delete item from database and return to wardrobe page
+  const handleDelete = async () => {
+    goBack();
+  };
+
   return (
     <div className={styles.container}>
       <WardrobeItemDetails />
-      <span onClick={goBack} className={styles.backButton}>
-        <HiOutlineArrowNarrowLeft size={35} />
-      </span>
+      <div className={styles.buttons}>
+        <span onClick={goBack} className={styles.button}>
+          <HiOutlineArrowNarrowLeft size={35} />
+        </span>
+        <span onClick={handleDelete} className={styles.button}>
+          <RiDeleteBinLine size={25} />
+        </span>
+      </div>
     </div>
   );
 }
