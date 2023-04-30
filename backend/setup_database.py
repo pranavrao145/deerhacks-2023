af6a2cbb22db62a724408b5f08c2925d7986128f
Shelley Chen <shelleychen318@gmail.com>
2023-04-30 02:50:59 -0400

    get request for getting favourite outfits

--- backend/setup_database.py
+++ backend/setup_database.py
@@ -6,15 +6,24 @@ with app.app_context():
 
     user = User(username="test", email="test", password="test")
     occasion = Occasion(name="Wedding")
-    clothing_item = ClothingItem(
+    clothing_item_1 = ClothingItem(
         clothing_type="Sweater", colour="Black", pattern="Striped", image_url="seiofh")
+    clothing_item_2 = ClothingItem(
+        clothing_type="T-Shirt", colour="Black", pattern="Striped", image_url="seiofh")
 
     db.create_all()
 
     db.session.add(user)
-    db.session.add(clothing_item)
+    db.session.add(clothing_item_1)
+    db.session.add(clothing_item_2)
 
-    user.clothing_items.append(clothing_item)
+    user.clothing_items.append(clothing_item_1)
+    user.clothing_items.append(clothing_item_2)
+
+    outfit = Outfit(favourite=True, image_url="https://static.wikia.nocookie.net/amogus/images/c/cb/Susremaster.png/",
+                    clothes=[clothing_item_1, clothing_item_2])
+
+    user.outfits.append(outfit)
 
     db.session.add(occasion)
 
--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,21 +1,27 @@
-import outfit from "../../assets/outfit.jpg";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import useToken from "../../utils/useToken";
 import styles from "./home.module.scss";
 
 export default function Home() {
-  const outfits = [
-    {
-      id: 1,
-      img: outfit,
-    },
-    {
-      id: 2,
-      img: outfit,
-    },
-    {
-      id: 3,
-      img: outfit,
-    },
-  ];
+  const [favOutfits, setFavOutfits] = useState([]);
+  const { token } = useToken();
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: `${process.env.REACT_APP_SERVER_URL}/get_favourite_outfits`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        setFavOutfits(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -23,9 +29,9 @@ export default function Home() {
       <h3>Ready to look lavish?</h3>
       <h3>Favourited Outfits</h3>
       <div className={styles.outfitsContainer}>
-        {outfits.map(({ id, img }) => (
+        {favOutfits.map(({ id, image_url }) => (
           <div className={styles.imageContainer} key={id}>
-            <img src={img} alt="outfit" />
+            <img src={image_url} alt="outfit" />
           </div>
         ))}
       </div>
